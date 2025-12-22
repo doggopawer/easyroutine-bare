6d7fb508e1be32e44dc56d983539dc58efc0c27a
dhkim <dhkim@mleverage.co.kr>
2025-12-22 20:20:01 +0900

    feat: CircleButton 생성

---
+++ src/components/CircleButton.tsx
@@ -0,0 +1,50 @@
+// CircleButton.tsx
+import React from 'react';
+import styled from 'styled-components/native';
+
+type CircleButtonProps = {
+  children: React.ReactNode;
+  width: number;
+  height: number;
+  onCircleButtonClick?: () => void;
+};
+
+type WrapperProps = {
+  $width: number;
+  $height: number;
+};
+
+const CircleButtonWrapper = styled.Pressable<WrapperProps>`
+  width: ${({ $width }) => $width}px;
+  height: ${({ $height }) => $height}px;
+  min-width: ${({ $width }) => $width}px;
+
+  border-radius: ${({ $width, $height }) => Math.min($width, $height) / 2}px;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  align-items: center;
+  justify-content: center;
+
+  box-shadow: ${({ theme }) => '0px 4px 8px rgba(0,0,0,0.15)'};
+`;
+
+const CircleButton: React.FC<CircleButtonProps> = ({
+  children,
+  width,
+  height,
+  onCircleButtonClick,
+}) => {
+  return (
+    <CircleButtonWrapper
+      accessibilityRole="button"
+      onPress={onCircleButtonClick}
+      $width={width}
+      $height={height}
+    >
+      {children}
+    </CircleButtonWrapper>
+  );
+};
+
+export default CircleButton;
